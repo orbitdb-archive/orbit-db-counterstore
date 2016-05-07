ac67355be73f177818321a95abb6597be6931c81
haad <haad@headbanggames.com>
2016-05-07 10:21:15 +0200

    First version

---
+++ src/CounterIndex.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Counter = require('crdts/src/G-Counter');
+
+class CounterIndex {
+  constructor(id) {
+    this._counter = new Counter(id);
+  }
+
+  get() {
+    return this._counter;
+  }
+
+  updateIndex(oplog, added) {
+    if(this._counter) {
+      added.filter((f) => f && f.payload.op === 'COUNTER')
+        .map((f) => Counter.from(f.payload.value))
+        .forEach((f) => this._counter.merge(f))
+    }
+  }
+}
+
+module.exports = CounterIndex;
