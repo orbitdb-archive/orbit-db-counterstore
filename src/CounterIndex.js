85b5f214cb2627fdd977f4ffefcb6a3e8b1cd49d
Richard Littauer <richard+github@burntfen.com>
2018-12-17 09:41:24 -0500

    chore: linting

--- src/CounterIndex.js
+++ src/CounterIndex.js
@@ -3,16 +3,16 @@
 const Counter = require('crdts/src/G-Counter')
 
 class CounterIndex {
-  constructor(id) {
+  constructor (id) {
     this._index = new Counter(id)
   }
 
-  get() {
+  get () {
     return this._index
   }
 
-  updateIndex(oplog) {
-    if(this._index) {
+  updateIndex (oplog) {
+    if (this._index) {
       const createCounter = e => Counter.from(e.payload.value)
       const mergeToIndex = e => this._index.merge(e)
       oplog.values.filter(e => e && e.payload.op === 'COUNTER')
