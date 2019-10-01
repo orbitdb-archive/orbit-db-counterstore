c27d05871082a6381d566bb3e152e5605afe2c08
shamb0t <shams.methnani@gmail.com>
2019-10-01 11:55:33 +0100

    rm keystore from ip

--- src/CounterStore.js
+++ src/CounterStore.js
@@ -5,11 +5,11 @@ const CounterIndex = require('./CounterIndex')
 const Counter = require('crdts/src/G-Counter')
 
 class CounterStore extends Store {
-  constructor (ipfs, id, dbname, options = {}) {
+  constructor (ipfs, identities, id, dbname, options = {}) {
     if (!options.Index) {
       Object.assign(options, { Index: CounterIndex })
     }
-    super(ipfs, id, dbname, options)
+    super(ipfs, identities, id, dbname, options)
     this._type = 'counter'
   }
 
