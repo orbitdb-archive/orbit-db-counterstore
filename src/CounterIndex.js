e709fcfd33a6dd1d03c81922d029c740240c5763
haad <haad@headbanggames.com>
2021-11-16 08:43:27 +0200

    Fix how counters are merged in CounterIndex

--- src/CounterIndex.js
+++ src/CounterIndex.js
@@ -13,15 +13,8 @@ class CounterIndex {
 
   updateIndex (oplog) {
     if (this._index) {
-      const mergeWith = (counter, otherCounters) => {
-        // Go through each counter in the other counter
-        Object.entries(otherCounters).forEach(([id, value]) => {
-          // Take the maximum of the counter value we have or the counter value they have
-          counter._counters[id] = Math.max(counter._counters[id] || 0, value)
-        })
-      }
       const getCounter = e => e.payload.value.counters
-      const mergeToIndex = e => mergeWith(this._index, e)
+      const mergeToIndex = _counters => this._index.merge({ _counters })
       oplog.values.filter(e => e && e.payload.op === 'COUNTER')
         .map(getCounter)
         .forEach(mergeToIndex)
